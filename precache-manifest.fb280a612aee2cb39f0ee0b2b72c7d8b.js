362c889808ec10afa6a113a3c64ef9ffed98aa9e
Sharon <sharon.yi@me.com>
2019-11-28 10:51:14 -0500

    Updates

---
+++ precache-manifest.fb280a612aee2cb39f0ee0b2b72c7d8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "340bbd7873370660828afd202eb43b56",
+    "url": "/react-gh-pages-test/index.html"
+  },
+  {
+    "revision": "15f5138e63250417e469",
+    "url": "/react-gh-pages-test/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "6fb0c9ca5cbd80fe2a91",
+    "url": "/react-gh-pages-test/static/js/2.e1806175.chunk.js"
+  },
+  {
+    "revision": "15f5138e63250417e469",
+    "url": "/react-gh-pages-test/static/js/main.067cb5ac.chunk.js"
+  },
+  {
+    "revision": "82206735460170dfcefe",
+    "url": "/react-gh-pages-test/static/js/runtime-main.4146c7c6.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-gh-pages-test/static/media/logo.25bf045c.svg"
+  }
+]);
